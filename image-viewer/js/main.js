a308299d448ee77a358350bf5a78814b22c41892
vinferno <vinsonfernandez@gmail.com>
2022-10-31 09:14:48 -0700

    image-viewer

---
+++ image-viewer/js/main.js
@@ -0,0 +1,15 @@
+let paramString = location.href.split('?')[1];
+let queryString = new URLSearchParams(paramString);
+
+for (let pair of queryString.entries()) {
+   console.log("Key is: " + pair[0]);
+   console.log("Value is: " + pair[1]);
+}
+
+console.log(queryString.get('file'));
+
+const img = document.querySelector('img');
+img.src = `../img/${queryString.get('file')}`;
+
+const p = document.querySelector('p');
+p.textContent = queryString.get('file');
