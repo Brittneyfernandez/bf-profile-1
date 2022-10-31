1b0ac1000e6fbec5c26f27ad2d2d432e5e4dbb44
vinferno <vinsonfernandez@gmail.com>
2022-10-31 11:02:14 -0700

    video

--- js/main.js
+++ js/main.js
@@ -82,3 +82,98 @@ function fall(circle) {
 document.addEventListener('click', (e) => {
     clicked = true;
 });
+
+document.querySelector('.carousel-control-prev').addEventListener('click', (e) => {
+    console.log( document.querySelector('.carousel-item.active video'))
+    document.querySelector('.carousel-item.active video').play();
+})
+document.querySelector('.carousel-control-next').addEventListener('click', (e) => {
+    
+    const current =  document.querySelector('.carousel-item.active video');
+    current.pause();
+    current.currentTime = 0;
+
+    setTimeout(() => {
+        const newCurrent =  document.querySelector('.carousel-item.active video');
+        newCurrent.currentTime = 0;
+        newCurrent.play();
+    }, 1000)
+})
+
+const targetNodes = document.querySelectorAll('.carousel-item');
+class ClassWatcher {
+
+    constructor(targetNode, classToWatch, classAddedCallback, classRemovedCallback) {
+        this.targetNode = targetNode
+        this.classToWatch = classToWatch
+        this.classAddedCallback = classAddedCallback
+        this.classRemovedCallback = classRemovedCallback
+        this.observer = null
+        this.lastClassState = targetNode.classList.contains(this.classToWatch)
+
+        this.init()
+    }
+
+    init() {
+        this.observer = new MutationObserver(this.mutationCallback)
+        this.observe()
+    }
+
+    observe() {
+        this.observer.observe(this.targetNode, { attributes: true })
+    }
+
+    disconnect() {
+        this.observer.disconnect()
+    }
+
+    mutationCallback = mutationsList => {
+        for(let mutation of mutationsList) {
+            if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
+                let currentClassState = mutation.target.classList.contains(this.classToWatch)
+                if(this.lastClassState !== currentClassState) {
+                    this.lastClassState = currentClassState
+                    if(currentClassState) {
+                        this.classAddedCallback()
+                    }
+                    else {
+                        this.classRemovedCallback()
+                    }
+                }
+            }
+        }
+    }
+}
+
+targetNodes.forEach(targetNode => {
+    window.addEventListener('scroll', () => {
+        const carousel = document.querySelector('#carouselExampleControls');
+        if (!isScrolledIntoView(carousel)) {
+            targetNode.querySelector('video').pause();
+        } else {
+            targetNode.querySelector('#carouselExampleControls .active video')?.play();
+        }
+    });
+const classWatcher = new ClassWatcher( targetNode, 'active', () => {
+
+    const video = targetNode.querySelector('video');
+    video.currentTime = 0;
+    video.play();
+}, () => {
+    const video = targetNode.querySelector('video');
+    video.currentTime = 0;
+    video.pause();
+});
+})
+
+function isScrolledIntoView(el) {
+    var rect = el.getBoundingClientRect();
+    var elemTop = rect.top;
+    var elemBottom = rect.bottom;
+
+    // Only completely visible elements return true:
+    var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+    // Partially visible elements return true:
+    //isVisible = elemTop < window.innerHeight && elemBottom >= 0;
+    return isVisible;
+}
